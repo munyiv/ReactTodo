f6ff62d606e7982a4070a1cdf70c75f0bf6a3206
student <audreymunyiva@gmail.com>
2021-08-05 09:26:59 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+// import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+   <div className="todo-app">
+       <TodoList/>
+   </div>
+
+
+  );
+}
+
+
+export default App;
